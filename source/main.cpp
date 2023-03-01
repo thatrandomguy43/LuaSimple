33de81c0c7f5f637534116d6cda6e7137a846cd1
thatrandomguy43 <asger.raegaard@gmail.com>
2023-03-01 15:00:35 +0100

    trying to make ASan work, adding a use-after-free

--- source/main.cpp
+++ source/main.cpp
@@ -31,6 +31,11 @@ int GetFunnyNumber (lua_State* L){
 
 int main(){
 
+int* sanitizer = new int;
+delete sanitizer;
+*sanitizer = 4;
+
+
 lua.DoString("for i=1,10 do print(\"hi\") end");
 
 lua.SetGlobal.Number(80, "some_number");
