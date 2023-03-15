00dce5fd54a9f490252c39e9d4adf1a328579e22
Asger <asger.raegaard@gmail.com>
2023-03-15 10:02:29 +0100

    added the build directory

---
+++ build/_deps/lua-subbuild/lua-populate-prefix/tmp/lua-populate-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Users/Bruger/Skrivebord/LuaSimple/build/_deps/lua-src"
+  "C:/Users/Bruger/Skrivebord/LuaSimple/build/_deps/lua-build"
+  "C:/Users/Bruger/Skrivebord/LuaSimple/build/_deps/lua-subbuild/lua-populate-prefix"
+  "C:/Users/Bruger/Skrivebord/LuaSimple/build/_deps/lua-subbuild/lua-populate-prefix/tmp"
+  "C:/Users/Bruger/Skrivebord/LuaSimple/build/_deps/lua-subbuild/lua-populate-prefix/src/lua-populate-stamp"
+  "C:/Users/Bruger/Skrivebord/LuaSimple/build/_deps/lua-subbuild/lua-populate-prefix/src"
+  "C:/Users/Bruger/Skrivebord/LuaSimple/build/_deps/lua-subbuild/lua-populate-prefix/src/lua-populate-stamp"
+)
+
+set(configSubDirs Debug)
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Users/Bruger/Skrivebord/LuaSimple/build/_deps/lua-subbuild/lua-populate-prefix/src/lua-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Users/Bruger/Skrivebord/LuaSimple/build/_deps/lua-subbuild/lua-populate-prefix/src/lua-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
