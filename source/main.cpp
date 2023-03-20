f2fedbdcda4ea11d511e062c4ad2acfa78e8cc76
thatrandomguy43 <asger.raegaard@gmail.com>
2023-03-20 21:40:12 +0100

    fixed most os my test code for my new system i realise i need a metatable registration func however

--- source/main.cpp
+++ source/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 LuaInstance lua;
 
-/*
+
 
 
 int StringOfAs(lua_State* Lua){
@@ -47,22 +47,22 @@ static int GetFunnyNumber (lua_State* Lua){
 };
 
 
-*/
+
 
 
 int main(){
-/*
+
 lua.DoString("for i=1,10 do print(\"hi\") end");
 
-lua.SetGlobal.Number(80, "some_number");
+lua.SetGlobal(80, "some_number");
 lua.DoString("print(some_number)");
 
 string goofy_words = "heya scott here";
-lua.SetGlobal.String(goofy_words, "some_string");
+lua.SetGlobal(goofy_words, "some_string");
 lua.DoString("print(some_string)");
 
 int magic_conch = 0;
-lua.SetGlobal.Boolean(magic_conch, "some_bool");
+lua.SetGlobal(magic_conch, "some_bool");
 lua.DoString("print(\"The earth is flat? \" .. tostring(some_bool))");
     
 
@@ -77,20 +77,17 @@ unordered_map<string,string> compound{
 
 lua.DoString("some_table = {} some_table.a_goofy_field = 20 some_table[2] = \"this is an index\" some_table.anotha_table = {} some_table.anotha_table.balls_status = \"itching\" ");
 
-lua_Table cpp_table = lua.GetGlobal.Table("some_table");
-cout << any_cast<lua_Number>(cpp_table.table_contents.at("a_goofy_field")) << endl;
+shared_ptr<lua_Table> cpp_table = get<shared_ptr<lua_Table>>(lua.GetGlobal("some_table"));
+cout << get<lua_Number>(cpp_table->table_contents.at("a_goofy_field")) << endl;
 
-cout << any_cast<string>(cpp_table.table_contents.at(2)) << endl;
+cout << get<string>(cpp_table->table_contents.at(2)) << endl;
 
-auto subtable = any_cast<lua_Table>(cpp_table.table_contents.at("anotha_table"));
-cout << any_cast<string>(subtable.table_contents.at("balls_status")) << endl; 
+auto subtable = get<shared_ptr<lua_Table>>(cpp_table->table_contents.at("anotha_table"));
+cout << get<string>(subtable->table_contents.at("balls_status")) << endl; 
 
-any ref_test = make_any<double>(69.420);
-double* retrieved_value_ref_test = any_cast<double>(&ref_test);
-*retrieved_value_ref_test = 54.321;
-cout << any_cast<double>(ref_test) << endl;
 
-lua.SetGlobal.CFunction(&StringOfAs, "scream");
+
+lua.SetGlobal(&StringOfAs, "scream");
 
 lua.DoString("help_me = scream(10) print(help_me)");
 
@@ -99,18 +96,18 @@ lua_Table my_metatable;
 int (*metamethod_ptr)(lua_State*) = &TestUserdata::GetFunnyNumber;
 TestUserdata my_object;
 
-my_metatable.table_contents["__call"] = make_any<lua_CFunction>(metamethod_ptr);
+my_metatable.table_contents["__call"] = metamethod_ptr;
 
 lua_Userdata userdata_object;
 any any_my_object = make_any<TestUserdata>(my_object);
 userdata_object.object = &any_my_object;
 userdata_object.metatable_name = "test_metatable";
 lua.SetGlobal.Metatable(my_metatable, "test_metatable");
-lua.SetGlobal.Userdata(userdata_object,"my_userdata");
+lua.SetGlobal(userdata_object,"my_userdata");
 
 lua.DoString("print(my_userdata(my_userdata))");
 
-lua_Function function_info = lua.GetGlobal.LuaFunction("print");
+lua_Function function_info = get<lua_Function>(lua.GetGlobal("print"));
 
 cout << boolalpha << "Stored in registry at: " << function_info.registry_key << " Argument count: " << function_info.argument_count << " Takes extra parameters: " << function_info.takes_extra_args << endl;
 
@@ -120,14 +117,14 @@ lua.DoFile("C:/Users/Asger/Desktop/programming/LuaSimple/source/funny file.lua")
  
 cout << any_cast<lua_Number>(lua.lua_return_values[0]) << endl;
 
-lua.DoFunction(function_info, {make_any<string>("my "), make_any<string>("balls "), make_any<string>("itch")});
+lua.DoFunction(function_info, {"my ", "balls ", "itch"});
 
 
 lua.DoString("function OnlyOneParam(argument) return (-argument) end");
 
-lua_Function one_param_lua_func = lua.GetGlobal.LuaFunction("OnlyOneParam");
-lua.DoFunction(one_param_lua_func, {make_any<lua_Number>(-6), make_any<lua_Number>(7)});
-cout << any_cast<lua_Number>(lua.lua_return_values[0]) << endl;
-*/
+lua_Function one_param_lua_func = get<lua_Function>(lua.GetGlobal("OnlyOneParam"));
+lua.DoFunction(one_param_lua_func, {-6, 7});
+cout << get<lua_Number>(lua.lua_return_values[0]) << endl;
+
 return 0;
 }
